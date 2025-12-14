901929de6a38c600c37649c96fe52e9890745432
mdc13256 <mdc570@york.ac.uk>
2025-12-14 19:31:58 +0000

    Initial commit

---
+++ core/src/main/java/io/github/omalsecond/Piece.java
@@ -0,0 +1,16 @@
+
+
+
+public class Piece{
+    private String type;
+    private boolean isWhite;
+    public Piece(String type, boolean isWhite){
+        this.type = type;
+        this.isWhite = isWhite;
+    }    public String getType(){
+        return type;
+    }
+    public boolean isWhite(){
+        return isWhite;
+    }
+}
